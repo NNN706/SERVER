d5ed6bcd9a1a8657854c682ceedd7388a5802a7e
NNN706 <150787520+NNN706@users.noreply.github.com>
2024-07-11 01:50:07 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Start
 sudo su
-screen -S APT apt update && apt upgrade -y
-screen -S OVPN apt install openvpn -y
+screen -S start
+apt update && apt upgrade -y
+apt install openvpn easy-rsa -y
+nano /etc/openvpn/server.conf
 
