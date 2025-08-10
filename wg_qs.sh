4ae1729436fd3017c04e0cb368065831cd495ea7
NNN706 <150787520+NNN706@users.noreply.github.com>
2025-08-10 12:58:19 +0300

    Update wg_qs.sh

--- wg_qs.sh
+++ wg_qs.sh
@@ -84,8 +84,8 @@ EOF
 
 echo "Пир добавлен в серверный конфиг $WG_SERVER_CONF с IP $CLIENT_IP"
 
-sudo wg-quick down wg0
-sudo wg-quick up wg0
+wg-quick down wg0
+wg-quick up wg0
 
 echo "WireGuard сервер перезапущен."
 echo "Новый клиент: $CLIENT_NAME с IP $CLIENT_IP"
